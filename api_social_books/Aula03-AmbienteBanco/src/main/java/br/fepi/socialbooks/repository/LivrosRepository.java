2881c1e5ded7890d0cf4d520b4cf6a9297978ef1
rasec-hsc <cesarhscn@hotmail.com>
2023-11-14 20:20:57 -0300

    lab 03

--- api_social_books/Aula03-AmbienteBanco/src/main/java/br/fepi/socialbooks/repository/LivrosRepository.java
+++
@@ -1,9 +0,0 @@
-package br.fepi.socialbooks.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.fepi.socialbooks.domain.Livro;
-
-public interface LivrosRepository extends JpaRepository<Livro, Long>{
-
-}
